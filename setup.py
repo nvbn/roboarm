e676c9e1618da4da835866b90ce1eeadd2bea5f0
nvbn <nvbn.rm@gmail.com>
2013-08-06 19:14:17 +0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '1.0'
 
 setup(name='roboarm',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description="Python library for controlling owi robotic arm edge",
+      long_description=open('README.rst').read(),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
-      url='',
+      url='https://github.com/nvbn/roboarm',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
